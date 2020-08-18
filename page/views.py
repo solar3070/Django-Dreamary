22a7b40fcb97afa62baed6ed7e324e32a22f877b
jun <solar3070@naver.com>
2020-08-18 16:51:15 +0900

    last

--- page/views.py
+++ page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Designer
 
 def home(request):
@@ -11,3 +11,43 @@ def introduce(request):
 def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk = designer_id)
     return render(request, 'detail.html', {'designer' : designer})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method == 'POST':
+        post = Designer()
+
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+
+def update(request, designer_id):
+    post = get_object_or_404(Designer, pk = designer_id)
+
+    if request.method == 'POST':
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+        
+    else:
+        return render(request, 'update.html', {'designer' : post})
+
+def delete(request, designer_id):
+    post = get_object_or_404(Designer, pk = designer_id)
+    post.delete()
+
+    return redirect('home')
